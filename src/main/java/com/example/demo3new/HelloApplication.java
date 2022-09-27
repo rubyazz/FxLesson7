806c8cf68aaf34abdb0819c9a0fb72ef269057ff
rubyazz <ersultan.abduvalov@gmail.com>
2022-09-27 09:12:07 +0600

    slider done

--- src/main/java/com/example/demo3new/HelloApplication.java
+++ src/main/java/com/example/demo3new/HelloApplication.java
@@ -1,23 +1,60 @@
 package com.example.demo3new;
 
+//package sample;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.StageStyle;
 
 public class HelloApplication extends Application {
+    double x,y = 0;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 370, 574);
-        stage.setTitle("FXLesson3");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
     }
 
+
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
+
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+//import java.io.IOException;
+//
+//public class HelloApplication extends Application {
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 370, 574);
+//        stage.setTitle("FXLesson3");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch();
+//    }
+//}
--- src/main/java/com/example/demo3new/WelcomeController.java
+++ src/main/java/com/example/demo3new/WelcomeController.java
@@ -1,32 +1,69 @@
 package com.example.demo3new;
 
+
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class WelcomeController {
+public class WelcomeController implements Initializable {
+
     @FXML
-    Button logoutbutton;
+    private ImageView Exit;
 
-    public void logoutbuttonClicked(ActionEvent event){
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("login.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 370, 574);
-            Stage stage = new Stage();
-            stage.setTitle("LOGIN PAGE");
-            stage.setScene(scene);
-            stage.show();
-            ((Node)(event.getSource())).getScene().getWindow().hide();
-        }
-        catch (IOException e) {
-            System.out.println(e.toString());
-        }
+    @FXML
+    private Label Menu;
+
+    @FXML
+    private Label MenuClose;
+
+    @FXML
+    private AnchorPane slider;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        Exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+        slider.setTranslateX(-176);
+        Menu.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-176);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(false);
+                MenuClose.setVisible(true);
+            });
+        });
+
+        MenuClose.setOnMouseClicked(event -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(-176);
+            slide.play();
+
+            slider.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                Menu.setVisible(true);
+                MenuClose.setVisible(false);
+            });
+        });
     }
 }
