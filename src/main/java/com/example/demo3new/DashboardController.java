3b77978fa5beff1f900951db776a085a5627838c
rubyazz <ersultan.abduvalov@gmail.com>
2022-09-27 12:32:05 +0600

    min max done

---
+++ src/main/java/com/example/demo3new/DashboardController.java
@@ -0,0 +1,4 @@
+package com.example.demo3new;
+
+public class DashboardController {
+}
--- src/main/java/com/example/demo3new/WelcomeController.java
+++ src/main/java/com/example/demo3new/WelcomeController.java
@@ -1,15 +1,24 @@
 package com.example.demo3new;
 
-
+//
+//import com.sun.javafx.tk.quantum.PaintRenderJob;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,15 +26,29 @@ public class WelcomeController implements Initializable {
 
     @FXML
     private ImageView Exit;
-
+    Stage stage = null;
     @FXML
     private Label Menu;
 
+    @FXML
+    private AnchorPane myPane;
     @FXML
     private Label MenuClose;
 
     @FXML
     private AnchorPane slider;
+
+    @FXML
+    private AnchorPane tmpPane;
+
+    @FXML
+    private ImageView exit_img, exit_img1;
+
+
+
+
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -34,6 +57,19 @@ public class WelcomeController implements Initializable {
             System.exit(0);
         });
         slider.setTranslateX(-176);
+
+
+
+
+        exit_img.setOnMouseClicked(event->{
+            stage = (Stage) myPane.getScene().getWindow();
+            if (stage.isMaximized()) {
+                stage.setMaximized(false);
+            } else {
+                stage.setMaximized(true);
+            }
+        });
+
         Menu.setOnMouseClicked(event -> {
             TranslateTransition slide = new TranslateTransition();
             slide.setDuration(Duration.seconds(0.4));
@@ -44,11 +80,18 @@ public class WelcomeController implements Initializable {
 
             slider.setTranslateX(-176);
 
-            slide.setOnFinished((ActionEvent e)-> {
+            slide.setOnFinished((ActionEvent e) -> {
                 Menu.setVisible(false);
                 MenuClose.setVisible(true);
             });
         });
+
+
+        exit_img1.setOnMouseClicked(MouseEvent -> {
+            ((Stage)((ImageView)MouseEvent.getSource()).getScene().getWindow()).setIconified(true);
+        });
+
+
 
         MenuClose.setOnMouseClicked(event -> {
             TranslateTransition slide = new TranslateTransition();
@@ -60,10 +103,31 @@ public class WelcomeController implements Initializable {
 
             slider.setTranslateX(0);
 
-            slide.setOnFinished((ActionEvent e)-> {
+            slide.setOnFinished((ActionEvent e) -> {
                 Menu.setVisible(true);
                 MenuClose.setVisible(false);
             });
         });
     }
+
+
+
+
+    @FXML
+    private  void itemMembres(ActionEvent event) throws IOException{
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
+
+        try {
+            Pane registerPane = fxmlLoader.load();
+            tmpPane.getChildren().clear();
+            tmpPane.getChildren().add(registerPane);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 }
