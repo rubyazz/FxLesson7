fcd693929778834620615fbdd25a30b055b4ab9d
rubyazz <ersultan.abduvalov@gmail.com>
2022-10-27 22:25:08 +0600

    lesson7

--- src/main/java/com/example/demo3new/DashboardController.java
+++ src/main/java/com/example/demo3new/DashboardController.java
@@ -1,4 +1,33 @@
 package com.example.demo3new;
 
-public class DashboardController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    ObservableList list = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<String> python;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        loadData();
+    }
+    private void loadData(){
+        list.removeAll(list);
+        String a="Python";
+        String b="Java";
+        String c="Haskell";
+        String d="C++";
+        list.addAll(a,b,c,d);
+        python.getItems().addAll(list);
+
+
+    }
 }
